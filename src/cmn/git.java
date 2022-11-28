927ca761d61e3f99b323ca76d273e85a4598a27a
Sun <Sun@123456.com>
2022-11-29 00:58:32 +0800

    second commit

--- src/cmn/git.java
+++ src/cmn/git.java
@@ -2,7 +2,8 @@ package cmn;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("git");
-        System.out.println("git");
+        System.out.println("git1");
+        System.out.println("git2");
+        System.out.println("git3");
     }
 }
